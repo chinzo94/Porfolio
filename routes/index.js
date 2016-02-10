9f620b76d8f7af9cc7554da24aa155090128305f
chinzo94 <chado94@rogers.com>
2016-02-10 17:10:27 -0500

    updated headers

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chad Ostler' });
-});
-
-/* GET about page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
-});
-
-/* GET projects page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
-});
-
-/* GET services page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Services' });
-});
-
-/* GET contact page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('index', { title: 'My Portfolio' });
 });
 
 module.exports = router;
