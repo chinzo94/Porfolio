a418f888e9ec1ea07da9b747801bccdec172e325
chinzo94 <chado94@rogers.com>
2016-02-10 16:54:08 -0500

    added text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Chad Ostler' });
+});
+
+/* GET about page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { header1: 'About Me' });
+});
+
+/* GET projects page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { header4: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { header3: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { header5: 'Contact Me' });
 });
 
 module.exports = router;
